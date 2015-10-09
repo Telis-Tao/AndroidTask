b371720921c158b7e6bc269d3fbe54604335244b
Telis <9simpsons@gmail.com>
2015-10-09 10:19:02 +0800

    Add function in DoubleCursorProgressBar with getter and setter for max value, cursors' position, drawableId and add basic comments. Normalize the namespace.

---
+++ app/src/main/java/com/example/telis/androidtask/MyRecycleActivity.java
@@ -0,0 +1,14 @@
+package com.example.telis.androidtask;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Telis on 2015/10/9.
+ */
+public class MyRecycleActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
